5568e03e8be344df88e2cfdc4fd3a52a11de86ba
Sonz <sonyarangraj@msn.com>
2021-07-25 22:59:54 -0500

    changes, added folders, added js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(require('./routes'));
+app.use(require('.'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
   useFindAndModify: false,
@@ -14,7 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-ap
   useUnifiedTopology: true
 });
 
-// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
